6f1836afeffc11cc44d12f1361bc5af1372dafea
Josh Mobley <josh@joshmobley.net>
2017-07-08 23:08:26 -0400

    adding components and mock data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { MapComponent } from './map/map.component';
+import { EventComponent } from './event/event.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    MapComponent,
+    EventComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'event/:id', component: EventComponent },
+      { path: '', component: ListComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/list/list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css'],
+  providers: [DataService]
+})
+export class ListComponent implements OnInit {
+
+  events: Object;
+
+  constructor(private dataService: DataService) { }
+
+  fetchData() {
+    this.dataService.getEvents().subscribe(
+      res => {
+        this.events = res.json();
+        console.log(this.events);
+      }
+    );
+  }
+
+  formatTime(t) {
+    // Check correct time format and split into components
+    let time = t.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [t];
+
+    if (time.length > 1) { // If time format correct
+      time = time.slice(1);  // Remove full string match value
+      time[5] = +time[0] < 12 ? 'am' : 'pm'; // Set AM/PM
+      time[0] = +time[0] % 12 || 12; // Adjust hours
+    }
+    return time.join(''); // return adjusted time or original string
+  }
+
+  ngOnInit() {
+    this.fetchData();
+  }
+
+}
